a80ad7b294a6569345d0e8c2be69a6f8707d3ffa
Aman <amanshaikh7102000@gmail.com>
2023-06-13 15:07:27 +0530

    Created getter and setter method for name vairable

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly rw = new RWOnly();
+       // rw.name = "Aman";
+        // error : 'name' has private access in 'com.driver.RWOnly'
+        rw.setName("Aman");
+        System.out.println(rw.getName());
+    }
 }
